42bae5105ae0f13d7e00a361c5eef066b7fee34b
MichaelSaracen <michael.saracen@bildung.cbm-bremen.de>
2025-02-23 10:21:41 +0100

    for_each

---
+++ basics/lambdas/algorithm/for_each.cpp
@@ -0,0 +1,20 @@
+#include "../funcs.h"
+
+using namespace funcs;
+using std::for_each, std::for_each_n, std::string;
+
+int main() {
+    IntVector vec = {1, 2, 3, 4, 5};
+    for_each(vec.begin(), vec.end(), [](int& x) { x = x * x; });
+    print(vec);
+
+
+    StringVector names = {"Alice", "Bob", "Charlie"};
+    for_each_n(names.begin(), names.size(), [](const string &name) {
+        print("Hallo", name, "\b!");
+    });
+
+    int sum = 0;
+    for_each(vec.begin(), vec.end(), [&sum](int x) { sum += x; });
+    print("Summe der Elemente:", sum);
+}
